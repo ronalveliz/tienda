e05f67136536d75f303f172e1972e536a81d1205
ronalveliz <ron.deivi1994@gmail.com>
2025-01-03 16:05:47 +0100

    se crea is login o no

--- frond/src/app/authentication/authentication.service.ts
+++ frond/src/app/authentication/authentication.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
+  // esto permite subcribirse y estar informado de  cuando el valor cambie
+  isloggedIn = new BehaviorSubject<boolean>(false);
+
   constructor() { }
 
   saveToken (token: string) {
     localStorage.setItem('token', token);
-  
+    this.isloggedIn.next(true);
   }
 }
